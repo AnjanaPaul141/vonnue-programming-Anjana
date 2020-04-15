1ccce6626871d2723abb2915b7a35077733022b9
Anjana Stephy <stephy.anjanab@gmail.com>
2020-04-15 22:22:55 +0530

    Function to find the sum of all elements of an array

---
+++ 004_sumofelements.js
@@ -0,0 +1,12 @@
+const Array = (element) => {
+    let Output = 0;
+    for (let i = 0; i < element.length; i++) {
+       Output = Output + element[i];
+    }
+    console.log(Output);
+ }
+    let element = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    Array(element);
+
+
+
